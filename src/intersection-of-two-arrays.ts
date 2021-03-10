2c58d88c8abf3b0c8a4b5ea5211651603cfae5b1
Urmzd <urmzds@gmail.com>
2021-03-10 17:35:40 -0400

    add intersection array || problem answer

---
+++ src/intersection-of-two-arrays.ts
@@ -0,0 +1,21 @@
+function intersect(nums1: number[], nums2: number[]): object {
+  let n1 = nums1;
+  let n2 = nums2;
+
+  if (nums1.length < nums2.length) {
+    n1 = nums2;
+    n2 = nums1;
+  }
+  const dups: {
+    [key: number]: number
+  } = {}
+
+  n1.forEach(v => dups[v] = (dups[v] ?? 0) + 1);
+
+  return n2.filter(v => {
+    const exists = dups[v] && dups[v] > 0;
+    dups[v] = dups[v] - 1;
+    return exists;
+  })
+}
+
