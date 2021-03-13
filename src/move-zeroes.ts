5d84600a523e77bb00469cf9018d164217beb2c0
Urmzd <urmzds@gmail.com>
2021-03-13 13:05:52 -0400

    add o(n) solution for two-sum

--- src/move-zeroes.ts
+++ src/move-zeroes.ts
@@ -1,5 +1,6 @@
 function moveZeroes(nums: number[]): void {
   const numsLength = nums.length;
+
   for (let i = numsLength - 1; i >= 0; i--) {
     if (!nums[i]) {
       const replaceWith = nums.splice(i, 1)[0];
---
+++ src/two-sum.ts
@@ -0,0 +1,20 @@
+function twoSum(nums: number[], target: number): number[] {
+  const dict = {};
+  
+  let result = []
+  nums.some((v, i) => {
+    if (dict.hasOwnProperty(target - v)) {
+      result = [dict[target-v], i];
+      return true;
+    }
+
+    dict[v] = i;
+
+    return false;
+  })
+
+  return result;
+} 
+
+console.log(twoSum([3,6,3], 6));
+
